23fd1fc349847faaad33caef915989fc7a68d4a1
Asya Bergal <asyabergal@gmail.com>
2014-10-04 22:29:24 -0400

    Tested account creation and wrote more necessary server operations.

--- server/server.go
+++ server/server.go
@@ -17,47 +17,81 @@ type Envelope struct{}
 
 type Server struct {
 	database *leveldb.DB
+	shutdown chan struct{}
 }
 
-func RunServer() error {
-	db, err := leveldb.OpenFile("github.com/andres-erbsen/chatterbox/database", nil)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+func RunServer(db *leveldb.DB, shutdown chan struct{}) error {
+	//TODO: It's possible we want to call defer db.Close() here and not in the calling method
 	server := &Server{
 		database: db,
+		shutdown: shutdown,
 	}
 	listener, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		return err
 	}
+	defer listener.Close()
+	go server.listenForServerShutdown(listener)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
 		go server.handleClient(conn)
+
+	}
+}
+
+func (server *Server) listenForServerShutdown(listener net.Listener) {
+	for _ = range server.shutdown {
+		listener.Close()
+	}
+}
+
+func (server *Server) listenForClientShutdown(connection net.Conn) {
+	for _ = range server.shutdown {
+		connection.Close()
 	}
 }
 
 //for each client, listen for commands
 func (server *Server) handleClient(connection net.Conn) error {
-	uid, newConnection, err := server.authenticateClient(connection)
+	uid, newConnection, err := authenticateClient(connection)
 	if err != nil {
 		return err
 	}
 	defer newConnection.Close()
+	go server.listenForClientShutdown(newConnection)
 	reader := io.NewDelimitedReader(newConnection, 16*1024)
+	writer := io.NewDelimitedWriter(newConnection)
 	command := new(Messages.ClientToServer)
 	for {
 		if err := reader.ReadMsg(command); err != nil {
 			return err
 		}
 		if command.CreateAccount != nil {
-			server.newUser(newConnection, uid)
+			if err := server.createAccount(newConnection, uid, writer); err != nil {
+				return err
+			}
 		}
 	}
+}
+
+func (server *Server) createAccount(connection net.Conn, uid Uid, writer io.Writer) error {
+	if err := server.newUser(connection, uid); err != nil {
+		if err := server.writeResponse(writer, Messages.ServerToClient_PARSE_ERROR.Enum()); err != nil {
+			return err
+		}
+		return err
+	}
+	return server.writeResponse(writer, Messages.ServerToClient_OK.Enum())
+}
+
+func (Server *Server) writeResponse(writer io.Writer, status *Messages.ServerToClient_StatusCode) error {
+	response := &Messages.ServerToClient{
+		Status: status,
+	}
+	return writer.WriteMsg(response)
 }
 
 func (server *Server) newUser(connection net.Conn, uid Uid) error {
@@ -65,6 +99,6 @@ func (server *Server) newUser(connection net.Conn, uid Uid) error {
 	return server.database.Put(append([]byte{'u'}, uid[:]...), []byte(""), nil)
 }
 
-func (server *Server) authenticateClient(connection net.Conn) (Uid, net.Conn, error) {
+func authenticateClient(connection net.Conn) (Uid, net.Conn, error) {
 	return [32]byte{}, connection, nil
 }
--- server/server_test.go
+++ server/server_test.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"code.google.com/p/gogoprotobuf/io"
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/andres-erbsen/chatterbox/proto"
+	"github.com/syndtr/goleveldb/leveldb"
+	"io/ioutil"
+	"net"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestAccountCreation(t *testing.T) {
-	err = RunServer()
+	dir, err := ioutil.TempDir("", "testdb")
 	if err != nil {
 		t.Error(err)
 	}
+	defer os.RemoveAll(dir)
+	db, err := leveldb.OpenFile(dir, nil)
+	defer db.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	shutdown := make(chan struct{})
+	go RunServer(db, shutdown)
 	conn, err := net.Dial("tcp", "localhost:8888")
+	defer conn.Close()
 	if err != nil {
 		t.Error(err)
 	}
-	writer := io.
-		t.Error("this test sucks")
+	writer := io.NewDelimitedWriter(conn)
+	command := &Messages.ClientToServer{
+		CreateAccount: proto.Bool(true),
+	}
+	err = writer.WriteMsg(command)
+	if err != nil {
+		t.Error(err)
+	}
+	handleResponse(conn, t)
+	close(shutdown)
+	iter := db.NewIterator(nil, nil)
+	if !iter.First() {
+		t.Error("Nothing in database")
+	}
+}
+
+func handleResponse(connection net.Conn, t *testing.T) error {
+	response := new(Messages.ServerToClient)
+	reader := io.NewDelimitedReader(connection, 16*100) //TODO: what should this buffer size be?
+	connection.SetDeadline(time.Now().Add(time.Second))
+	if err := reader.ReadMsg(response); err != nil {
+		t.Error(err)
+	}
+	if *response.Status == Messages.ServerToClient_PARSE_ERROR {
+		t.Error("Server failed to update database.")
+	}
+	return nil //We did it!
 }
