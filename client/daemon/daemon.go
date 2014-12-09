19e5af0e38fe73ad986c12b981eb4e459bc77d9e
David Kaufman <chessdog@gmail.com>
2014-12-09 03:22:10 -0500

    Made the daemon a library; created chatterboxd (executable)

--- client/daemon/daemon.go
+++ client/daemon/daemon.go
@@ -1,7 +1,7 @@
 // client daemon
 //   watches the file system for new messages --> sends them
 //   communicates with the server --> receive new messages
-package main
+package daemon
 
 import (
 	"code.google.com/p/go.exp/fsnotify"
@@ -15,15 +15,7 @@ func GetRootDir() string {
 	return temporaryConstantRootDirectory
 }
 
-func main() {
-	err := mainReturnErr()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-}
-
-func mainReturnErr() error {
+func Run() error {
 	conf := Config{
 		RootDir:    GetRootDir(),
 		Time:       time.Now,
