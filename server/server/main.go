b7c2ae1f0365bb8f42188f3266fd3fe0b7df75bd
Andres Erbsen <andres@krutt.org>
2014-12-10 00:53:24 -0500

    server binary; keygen

---
+++ server/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/andres-erbsen/chatterbox/server"
+	"github.com/syndtr/goleveldb/leveldb"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Fprintf(os.Stderr, "USAGE: %s <sk> <pk> <dbdir>", os.Args[0])
+		os.Exit(2)
+	}
+	db, err := leveldb.OpenFile(os.Args[3], nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	skBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sk, pk [32]byte
+	copy(sk[:], skBytes)
+	copy(pk[:], pkBytes)
+	server.StartServer(db, nil, &pk, &sk)
+	select {}
+}
