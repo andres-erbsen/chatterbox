716e02482f0dd6dc2870787609cbead8ca6a1c2e
Fritjof Bornebusch <fritjof@alokat.org>
2015-02-02 13:47:06 +0100

    Write private and public key into single files with the correct file permissions, and don't write them to stdout or stderr. Previously created files, will not be overwritten.

--- transport/transport-keygen/main.go
+++ transport/transport-keygen/main.go
@@ -3,18 +3,47 @@ package main
 import (
 	"crypto/rand"
 	"golang.org/x/crypto/nacl/box"
+	"io/ioutil"
+	"log"
+	"fmt"
+	"path"
 	"os"
 )
 
+const PRIVATE_KEY string = "private_key"
+const PUBLIC_KEY string = "public_key"
+
 func main() {
+	var dir string
+	if len(os.Args) < 2 {
+		dir = "."
+	} else {
+		dir = os.Args[1]
+	}
+
+	privfile := path.Join(dir, PRIVATE_KEY)
+	pubfile := path.Join(dir, PUBLIC_KEY)
+
+	if _, err := os.Stat(privfile); err == nil {
+		fmt.Fprintf(os.Stderr, "%s already exists\n", privfile)
+		os.Exit(1)
+	}
+	if _, err := os.Stat(pubfile); err == nil {
+		fmt.Fprintf(os.Stderr, "%s already exists\n", pubfile)
+		os.Exit(1)
+	}
+
 	pk, sk, err := box.GenerateKey(rand.Reader)
 	if err != nil {
+		log.Fatal(err)
 		os.Exit(2)
 	}
-	if _, err := os.Stdout.Write(pk[:]); err != nil {
+	if err := ioutil.WriteFile(privfile, sk[:], 0600); err != nil {
+		log.Fatal(err)
 		os.Exit(3)
 	}
-	if _, err := os.Stderr.Write(sk[:]); err != nil {
+	if err := ioutil.WriteFile(pubfile, pk[:], 0644); err != nil {
+		log.Fatal(err)
 		os.Exit(4)
 	}
 }
