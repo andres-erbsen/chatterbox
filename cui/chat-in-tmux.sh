5a1feb7265233e046aff7c593ce71ba4d4e705f7
Andres Erbsen <andres@krutt.org>
2015-02-15 13:13:05 -0500

    more cui improvements...

--- cui/chat-in-tmux.sh
+++ cui/chat-in-tmux.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 tmux split-window -v -l 3
-tmux send-keys -t 1 "./chat-incoming.sh \"$1\" \"$2\"" enter
+tmux send-keys -t 1 "\"$DIR/chat-incoming.sh\" \"$1\" \"$2\"" enter
 tmux swap-pane -D -t 1
 tmux select-pane -t 1
-./chat-outgoing.sh "$1" "$2"
+"$DIR/chat-outgoing.sh" "$1" "$2"
 tmux kill-pane -t 0
