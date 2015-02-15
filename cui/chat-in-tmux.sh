f7f78418f43555c933857d5b9684a5f95187baf5
Andres Erbsen <andres@krutt.org>
2015-02-15 12:54:59 -0500

    cui improvements

--- cui/chat-in-tmux.sh
+++ cui/chat-in-tmux.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 tmux split-window -v -l 3
-tmux send-keys -t 1 "env \"EDITOR=$EDITOR\" ./chat-outgoing.sh \"$1\" \"$2\"" enter
+tmux send-keys -t 1 "./chat-incoming.sh \"$1\" \"$2\"" enter
+tmux swap-pane -D -t 1
 tmux select-pane -t 1
-exec ./chat-incoming.sh "$1" "$2"
+./chat-outgoing.sh "$1" "$2"
+tmux kill-pane -t 0
